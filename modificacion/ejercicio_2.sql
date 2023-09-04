6cb64b568e4d4a2e04146034087ac3d8b3ab3a37
Matias Mosquera <matiasarielmosquera@gmail.com>
2023-09-04 14:58:05 -0300

    ejercicio2

--- modificacion/ejercicio_2.sql
+++ modificacion/ejercicio_2.sql
@@ -1,7 +1,6 @@
+
 INSERT INTO user (username, password)
-VALUES ("tapon", "loca"), ("mateo", "mateo01")
- 
- INSERT INTO user (username)
- SELECT 1 as p.password, u.username
-     FROM username p JOIN user u
-     ON p.username = u.password
+VALUES ("Roca", "Loca");
+
+INSERT INTO user (username, password)
+VALUES ("Mono", "Mata");
