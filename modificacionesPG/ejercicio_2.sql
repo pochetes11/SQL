1aada82b789f0dde4f45d4d8e473946e63480e5c
pochetes11 <matiasarielmosquera@gmail.com>
2023-09-25 17:07:47 -0300

    modificacionesPG

---
+++ modificacionesPG/ejercicio_2.sql
@@ -0,0 +1,5 @@
+INSERT INTO "user" (username, password)
+VALUES ('Roca', 'Loca');
+
+INSERT INTO "user" (username, password)
+VALUES ('Mono', 'Mata');
---
+++ modificacionesPG/ejericicio_3.sql
@@ -0,0 +1,7 @@
+DELETE FROM "user" WHERE username = 'loca';
+
+DELETE FROM "user" WHERE username = 'caco';
+
+DELETE FROM "user" WHERE username = 'mateo';
+
+DELETE FROM "user" WHERE username = 'mateo01';
